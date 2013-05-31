d154831343d6869388c3c8af2ac2a72f40bf1746
AION <t.viteevanich@gmail.com>
2013-05-31 16:35:32 +0700

    Create new Blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'f81832c6c936d1b9529cf2a9dd360f93e6403be990e8256fc30931cc4c38058c1f0d9df57296b41c9b8e58013e1096907a064b1740a2eefd9df0e9b485014b10'
